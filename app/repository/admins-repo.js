dc87ab424a0552f86831bf9a884b1a65ed6721a9
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-23 18:50:35 +0300

    a

--- app/repository/admins-repo.js
+++ app/repository/admins-repo.js
@@ -1,1 +1,11 @@
 import Admin from "../models/admins";
+export default class AdminRepo {
+    getAdmin() {
+        return Admin.find()
+    }
+    addAccount(admin) {
+        return Admin.create(admin)
+    }
+
+}
+
