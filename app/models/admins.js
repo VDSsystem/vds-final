f0da026efacd248c45d6f91b702fe9f9caabcf69
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-23 18:37:13 +0300

    admins model

--- app/models/admins.js
+++ app/models/admins.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+const Schema = mongoose.Schema
+const options = {toJSON: {virtuals: true}}
+const adminSchema = new Schema({
+    email : {
+        type : String,
+        required : [true , 'email is a required field'],
+    },
+    password : {
+        type: String,
+        required : [true , 'password is a required field'],
+    },
+}, options)
+export default mongoose.model('Admins', adminSchema)
